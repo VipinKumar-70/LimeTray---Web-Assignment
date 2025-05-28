7f3806c207194301cd07a617f8cd145578d611b6
VipinKumar-70 <vk20140074250@gmail.com>
2025-05-29 00:34:54 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+  // Hamburger toggle
+  const $hamburger = $(".hamburger");
+  const $navLinks = $(".nav-links");
+
+  function toggleMenu() {
+    $navLinks.toggleClass("open");
+    const isOpen = $hamburger.hasClass("open");
+
+    $hamburger
+      .toggleClass("open")
+      .html(
+        isOpen ? '<i class="fas fa-bars"></i>' : '<i class="fas fa-times"></i>'
+      );
+  }
+
+  $hamburger.on("click keypress", function (e) {
+    if (e.type === "click" || e.key === "Enter") {
+      toggleMenu();
+    }
+  });
+
+  // Slider functionality
+  let $slides = $(".slide");
+  let current = 0;
+  let slideInterval;
+
+  function showSlide(index) {
+    $slides.removeClass("active").css("opacity", 0);
+    $slides.eq(index).addClass("active").css("opacity", 1);
+  }
+
+  function nextSlide() {
+    current = (current + 1) % $slides.length;
+    showSlide(current);
+  }
+
+  $(".next").on("click", function () {
+    nextSlide();
+    resetAutoplay();
+  });
+
+  $(".prev").on("click", function () {
+    current = (current - 1 + $slides.length) % $slides.length;
+    showSlide(current);
+    resetAutoplay();
+  });
+
+  function startAutoplay() {
+    slideInterval = setInterval(nextSlide, 2500); // change every 2.5s
+  }
+
+  function resetAutoplay() {
+    clearInterval(slideInterval);
+    startAutoplay();
+  }
+
+  showSlide(current);
+  startAutoplay();
+});
